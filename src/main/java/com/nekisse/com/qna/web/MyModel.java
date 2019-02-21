d46b0d3030ba49cec6199fddbbb63d20d81796dc
nekisse <lsh891224@gmail.com>
2019-02-21 19:42:13 +0900

    mac qna   mustache test

---
+++ src/main/java/com/nekisse/com/qna/web/MyModel.java
@@ -0,0 +1,14 @@
+package com.nekisse.com.qna.web;
+
+public class MyModel {
+
+    private String name;
+
+    public MyModel(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}

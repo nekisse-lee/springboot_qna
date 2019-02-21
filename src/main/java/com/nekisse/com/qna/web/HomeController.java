5bb0483ccdda287c969e5096f1bdc7ea952c3add
nekisse <lsh891224@gmail.com>
2019-02-21 18:25:44 +0900

    mac springboot qna 부트스트랩,  form페이지 구현

---
+++ src/main/java/com/nekisse/com/qna/web/HomeController.java
@@ -0,0 +1,13 @@
+package com.nekisse.com.qna.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}

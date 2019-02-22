7148764bdd5e765440af3690dcdbaf2a81585d24
nekisse <lsh891224@gmail.com>
2019-02-22 17:46:06 +0900

    mac springboot h2 데이터베이스,  유저정보 수정 구현 완

--- src/main/java/com/nekisse/com/qna/web/UserController.java
+++ src/main/java/com/nekisse/com/qna/web/UserController.java
@@ -1,30 +1,58 @@
 package com.nekisse.com.qna.web;
 
+import com.nekisse.com.qna.domain.User;
+import com.nekisse.com.qna.domain.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
 
-    private List<User> users = new ArrayList<>();
+
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
 
-    @PostMapping("/create")
+    @GetMapping("/form")
+    public String form() {
+        return "user/form";
+    }
+
+    @PostMapping("")
     public String home(User user) {
-        users.add(user);
-        System.out.println("users = " + users);
-        return "redirect:/list";
+        System.out.println("user = " + user);
+        userRepository.save(user);
+        return "redirect:/users";
     }
 
 
-    @GetMapping("list")
+    @GetMapping("")
     public String list(Model model) {
-        model.addAttribute("users", users);
-        return "list";
+        model.addAttribute("users", userRepository.findAll());
+        return "user/list";
     }
+
+
+    @GetMapping("/{id}/form")
+    public String updateForm(@PathVariable Long id, Model model) {
+        User user = userRepository.getOne(id);
+        System.out.println("user = " + user);
+        model.addAttribute("user", user);
+        return "/user/updateForm";
+    }
+
+    @PutMapping("/{id}")
+    public String update(@PathVariable Long id , User newUser) {
+        User user = userRepository.getOne(id);
+        user.update(newUser);
+        userRepository.save(user);
+        return "redirect:/users";
+    }
+
+
 }

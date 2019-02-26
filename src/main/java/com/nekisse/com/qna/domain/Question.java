04425826e63bec811fad0c60d3e15e23cfae855d
nekisse <lsh891224@gmail.com>
2019-02-26 13:19:36 +0900

    mac springboot qna    step5.2--질문하기 구현, 질문상세보기 구현

--- src/main/java/com/nekisse/com/qna/domain/Question.java
+++ src/main/java/com/nekisse/com/qna/domain/Question.java
@@ -1,6 +1,8 @@
 package com.nekisse.com.qna.domain;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Entity
 public class Question {
@@ -10,20 +12,45 @@ public class Question {
     private Long id;
 
     @ManyToOne
-    @JoinColumn(foreignKey = )
+    @JoinColumn(foreignKey = @ForeignKey(name = "fk_question_writer"))
     private User writer;
 
     private String title;
 
     private String contents;
 
+    private LocalDateTime createDate;
+
+    public User getWriter() {
+        return writer;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public LocalDateTime getCreateDate() {
+        return createDate;
+    }
 
     public Question() {
     }
 
-    public Question(String writer, String title, String contents) {
+    public Question(User writer, String title, String contents) {
         this.writer = writer;
         this.title = title;
         this.contents = contents;
+        this.createDate = LocalDateTime.now();
+    }
+
+    public String getFormattedCreateDate() {
+        if (createDate == null) {
+            return "";
+        }
+        return createDate.format(DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss"));
     }
 }

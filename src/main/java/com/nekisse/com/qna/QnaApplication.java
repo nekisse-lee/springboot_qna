f8c2736745281077df9fceec4359388ff0c0d83c
nekisse <lsh891224@gmail.com>
2019-02-27 17:49:59 +0900

    mac springboot qna step6.6-- Swagger 라이브러리를 통한  API 문서화 및 테스트.

--- src/main/java/com/nekisse/com/qna/QnaApplication.java
+++ src/main/java/com/nekisse/com/qna/QnaApplication.java
@@ -2,14 +2,49 @@ package com.nekisse.com.qna;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableSwagger2
 public class QnaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(QnaApplication.class, args);
     }
 
+
+
+    @Bean
+    public Docket newsApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+            .groupName("qna")
+            .apiInfo(apiInfo())
+            .select()
+            .paths(PathSelectors.ant("/api/**"))
+            .build()
+            ;
+
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+            .title("My Qna Api")
+            .description("My Qna Api")
+            .termsOfServiceUrl("termsOfServiceUrl")
+            .contact("contact")
+            .license("license")
+            .licenseUrl("licenseUrl")
+            .version("2.0")
+            .build();
+    }
 }
